12165e92de73550ba7e01452c2cd7c32a7792818
Prytula Olexandr <liga5872@gmail.com>
2024-04-16 14:35:03 +0300

    add task solution

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,20 @@
-// export const Person = ({ person }) => ();
+export const Person = ({ person }) => {
+  const hasPartner =
+    person.sex === 'f' ? (
+      <p className="Person__partner">{person.partnerName} is my husband</p>
+    ) : (
+      <p className="Person__partner">{person.partnerName} is my wife</p>
+    );
+
+  return (
+    <section className="Person">
+      <h2 className="Person__name">My name is {person.name}</h2>
+      {person.age ? <p className="Person__age">I am {person.age}</p> : null}
+      {person.isMarried === true ? (
+        hasPartner
+      ) : (
+        <p className="Person__partner">I am not married</p>
+      )}
+    </section>
+  );
+};
